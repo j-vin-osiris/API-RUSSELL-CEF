1ffd845142a92f4f5c4e717b5cef7ce4969b713c
jvin <jvboetto@gmail.com>
2025-02-27 19:11:49 +0100

    Add EJS views for home, login, and dashboard pages

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.set("view engine", "ejs");
 
 // Routes
 app.use("/api/catways", require("./routes/catways"));
 app.use("/api", require("./routes/reservations"));
 app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
+// Route pour la page d'accueil
+app.get("/", (req, res) =>
+  res.render("index", { title: "Port de Plaisance Russell" })
+);
+
+app.get("/login", (req, res) => res.render("login"));
+
+app.get("/dashboard", (req, res) => res.render("dashboard"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
