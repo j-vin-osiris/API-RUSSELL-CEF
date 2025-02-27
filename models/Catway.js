85c6ad8d2e77a8a86e2bd2eb5f44c234b5d57d3b
jvin <jvboetto@gmail.com>
2025-02-27 17:32:06 +0100

    Create Mongoose models for User, Catway, and Reservation

---
+++ models/Catway.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const CatwaySchema = new mongoose.Schema({
+  catwayNumber: { type: Number, required: true, unique: true },
+  catwayType: { type: String, enum: ["long", "short"], required: true },
+  catwayState: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Catway", CatwaySchema);
